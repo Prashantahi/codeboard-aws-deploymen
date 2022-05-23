4d5c5e16fa1338b828f06b5474f19a814ab80e58
SumitBandal <sumit.bandal@zpluscybertech.com>
2022-05-23 15:27:37 +0530

    prashant

---
+++ src/main/java/com/example/codeboardaws/controller/PersonController.java
@@ -0,0 +1,14 @@
+package com.example.codeboardaws.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+    @RequestMapping("/getName")
+    public String getName(){
+        return "Prashant";
+    }
+
+}
